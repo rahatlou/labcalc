ab84515754b32981d74d50e7d93be8e20900ee87
Shahram Rahatlou <rahatlou@gmail.com>
2024-11-13 15:20:34 +0100

    primi esempi banali

---
+++ material/secondo.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main() {
+
+  int n;
+  n = 3;
+
+  printf("ciao a tutti");
+
+  printf("%d", n);
+
+  printf("n = %d", n);
+
+  printf("\n\n");
+
+  printf("%d\n", n);
+  printf("n = %d\n", n);
+
+}
