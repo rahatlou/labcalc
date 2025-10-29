7c48ce7d97aaf9d87f9d5fe54390a579398edf54
Shahram Rahatlou <rahatlou@gmail.com>
2025-10-29 11:49:18 +0100

    ricerca primi 100 numeri primi

--- material/primi.c
+++ material/primi.c
@@ -7,17 +7,24 @@
 int main() {
 
   int i,j,jMax;
-  printf("numeri primi tra 1 e %d:\n", N);
+  printf("Numeri primi tra 1 e %d:\n", N);
+
+  // ciclo sui numeri da 1 a N
   for(i=1; i<=N; i++) {
+
+    // valori tra cui cercare divisori
     j = 2;
     jMax = (int)sqrt(i)+1;
 
+    // aumenta j fino jMax  solo se i non divisibile per j
     while( (j<jMax) && (i%j) ) {
       j++;
     }
+    
     if(j == jMax) printf("%d\n", i);
 
-  }
+  } // ciclo for
+
+}  // main
 
 
-}
