49215e1860bbbe90b90fbd0bfe7bfed2f64ba3e9
Shahram Rahatlou <rahatlou@gmail.com>
2025-11-18 21:12:10 +0100

    aggiornato stampa info e aggiunto versione con puntatori

--- material/bubblePtr.c
+++ material/bubblePtr.c
@@ -12,22 +12,25 @@ int main() {
 
    //stampa array
    printf("pre-ordinamento data = { ");
-   for(i=0; i<LEN; i++) printf("%d ", data[i]);
+   for(i=0; i<LEN; i++) printf("%d ", *(data+i));
    printf("}\n\n");
 
    
-   for(i=0; i<LEN; i++) {
+   // ciclo i sugli elementi dall'inizio
+   for(i=0; i<LEN; i++) { 
+
+     // ciclo j sugli elemento da ultimo fino a i+1
      for(j=LEN-1; j>i; j--) {
 
        printf("i: %d \t j: %d \t data[%d]: %d \t data[%d]: %d\n",
-	       i, j, j-1, *(data+j-1), j, *(data+j));
+	      i, j, j-1, *(data+j-1), j, *(data+j));
 
-       if(data[j-1] > data[j]) {
+       if(*(data+j-1) > *(data+j)) {
 	 printf("==> scambia data[%d] con data[%d]\n\n", j, j-1);
-	 temp = data[j];
-	 data[j] = data[j-1];
-	 data[j-1] = temp;
-       } // scambio se necessario
+	 temp = *(data+j);
+	 *(data+j) = *(data+j-1);
+	 *(data+j-1) = temp;
+       } // fine scambio 
        
      }  // ciclo j sugli elementi successivi a i
      
@@ -35,7 +38,7 @@ int main() {
 
    //stampa array
    printf("dopo ordinamento data = { ");
-   for(i=0; i<LEN; i++) printf("%d ", data[i]);
+   for(i=0; i<LEN; i++) printf("%d ", *(data+i));
    printf("}\n\n");
 
 
